dee216bcf3e11161619fd0e421d3b8a3a7c3e7ac
oshinkoshinko <aiueokakatyuka.9@gmail.com>
2020-12-15 09:44:55 +0000

    [Update2] 4-10

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
-  root to: 'homes#top'
+
+devise_for :users
+  resources :users,only: [:show,:index,:edit,:update]
+  resources :books
+  root 'homes#top'
+  get 'home/about' => 'homes#show'
 end
