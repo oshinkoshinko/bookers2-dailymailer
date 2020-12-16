e6c45fffa6331b3e12308c3a040d669198efcdd1
oshinkoshinko <aiueokakatyuka.9@gmail.com>
2020-12-16 05:53:32 +0000

    [Update3] 4-10

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,13 @@
 class UsersController < ApplicationController
   def index
     @users = User.all
+    @user = current_user
     @book = Book.new
   end
 
   def show
     @user = User.find(params[:id])
+    @books = Book.all
     @book = Book.new
   end
 
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'f534ba80ca52094450d9d8239e259b21100ff7996eab19087a503453db771c28e06e6c73e49998330c6c0a8c8405478d04204a6c6b65b2bc29480ace0bff56fb'
