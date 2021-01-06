207a2f371445afbfc4a5e81a74e77b70015e7a23
oshinkoshinko <aiueokakatyuka.9@gmail.com>
2021-01-06 13:36:12 +0000

    Make relationship model

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,27 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy
   has_many :book_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
+  has_many :active_relationships, class_name: "Relationship",
+              foreign_key: "follower_id",
+                dependent: :destroy
+  has_many :following, through::active_relationships, source::followed
+  has_many :passive_relationships, class_name: "Relationship",
+              foreign_key: "followed_id",
+                dependent: destroy
+  has_many :followers, through::passiive_relationships, source::follower
+
+
+  def follow(user)
+    following << user
+  end
+
+  def unfollow(user)
+    active_relationships.find_by(followed_id: user.id).destroy
+  end
+
+  def following?(user)
+    following.include?(user)
+  end
 
   attachment :profile_image
 
