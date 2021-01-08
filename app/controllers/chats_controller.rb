ddf7b7dd66149c2c5c09e573f6ed9c9047903212
oshinkoshinko <aiueokakatyuka.9@gmail.com>
2021-01-08 16:30:59 +0000

    Finish test9

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,6 +1,5 @@
 class ChatsController < ApplicationController
   def show
-    binding.pry
     @user = User.find(params[:id])
     rooms = current_user.user_rooms.pluck(:room_id)
     user_rooms = UserRoom.find_by(user_id: @user.id, room_id: rooms)
